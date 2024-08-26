f7609a557f066b5e8f9374d8dcc747bf2e2e6471
Sarbajit Mukhopadhyaya <sarbajit.m@icat.ac.in>
2024-08-26 15:28:29 +0530

    Game flow demo updated
    Scenes added, manager changed. SOs added.

--- Project/PowerUpPingPong/Assets/_scripts/GameStateManager.cs
+++ Project/PowerUpPingPong/Assets/_scripts/GameStateManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public enum GameStates
 {
@@ -12,7 +14,10 @@ public enum GameStates
 public class GameStateManager : MonoBehaviour
 {
     [SerializeField] private GameStates _currentState;
-    
+    [SerializeField] private bool _isChangingStates;
+    [SerializeField] private int _index;
+    [Header("Events"), Space(4)]
+    public UnityEvent OnStateChanged;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,21 +28,7 @@ public class GameStateManager : MonoBehaviour
     // Update is called once PER FRAME
     void Update()
     {
-        switch (_currentState)
-        {
-            case GameStates.menu:
-                //pop menu UI, disable input, start music
-                break;
-            case GameStates.gameplay:
-                //enable input, fade out menu UI, start SFX
-                break;
-            case GameStates.results:
-                //disable input, fade in Result UI / Game Over UI, change music
-                break;
-            case GameStates.pause:
-                //Disable input, fade in Pause menu UI, pause music
-                break;
-        }
+        ChangeStateContinous(_currentState);
     }
 
     private void LateUpdate()
@@ -45,28 +36,83 @@ public class GameStateManager : MonoBehaviour
         //State Change Based on Input
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            //start game
             _currentState = GameStates.menu;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
+            //start gameplay
             _currentState = GameStates.gameplay;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            //end gameplay
             _currentState = GameStates.results;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
+            //pause game
             _currentState = GameStates.pause;
         }
     }
 
     //This can be called from Outside the script, Ideally a Button (UnityEvent)
-    public void ChangeState(GameStates state)
+    public void ChangeState()
     {
-        _currentState = state;
+        _isChangingStates = true;
+    }
+
+    private void ChangeStateContinous(GameStates state) 
+    {
+        if (_isChangingStates)
+        {
+            switch (state)
+            {
+                case GameStates.menu:
+                    //On Game Start
+                    //pop menu UI, disable input, start music
+                    //main menu already Loaded
+                    _index = 0;
+                    break;
+                case GameStates.gameplay:
+                    //enable input, fade out menu UI, start SFX
+                    //load level scenes
+                    _index++;
+                    LoadScene(_index);
+                    break;
+                case GameStates.results:
+                    //On Game End
+                    //disable input, fade in Result UI / Game Over UI, change music
+                    //load last scene
+                    LoadScene(SceneManager.sceneCountInBuildSettings - 1);
+                    break;
+                case GameStates.pause:
+                    //Disable input, fade in Pause menu UI, pause music
+                    break;
+            }
+
+            OnStateChanged?.Invoke();
+            Debug.Log("changed states");
+
+            _isChangingStates = false;
+        }
+    }
+
+    private void LoadScene(int sceneId)
+    {
+        if(SceneManager.sceneCount < 2)
+        {
+            SceneManager.LoadScene(sceneId, LoadSceneMode.Additive);
+        }
+
+        else
+        {
+            Debug.Log("Can't load more than 2 scenes.", this);
+            SceneManager.UnloadSceneAsync(sceneId - 1);
+            SceneManager.LoadSceneAsync(sceneId, LoadSceneMode.Additive);
+        }
     }
 }
